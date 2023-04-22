624cb2ba93f0bcd3721f5a5486203b0f5f3aab14
abhilasha2418 <abhilashapimpalgaonkar18@gmail.com>
2023-04-22 20:36:36 +0530

    REST changes

--- app/urls.py
+++ app/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import GoogleCalendarInitView, GoogleCalendarRedirectView
 
 urlpatterns = [
-    path('calendar/init/', GoogleCalendarInitView.as_view()),
-    path('calendar/redirect/', GoogleCalendarRedirectView.as_view()),
+    path('rest/v1/calendar/init/', GoogleCalendarInitView.as_view(), name='google_calendar_init'),
+    path('rest/v1/calendar/redirect/', GoogleCalendarRedirectView.as_view(), name='google_calendar_redirect'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import redirect
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from django.shortcuts import redirect
 from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import Flow
@@ -10,7 +11,6 @@ CLIENT_SECRETS_FILE = '/home/katniss/convin/GcalenderIntegration/app/credentials
 
 # Define redirect URI for OAuth2
 REDIRECT_URI = 'http://localhost:3000/rest/v1/calendar/redirect/'
-
 
 
 class GoogleCalendarInitView(APIView):
@@ -26,11 +26,9 @@ class GoogleCalendarInitView(APIView):
             redirect_uri=REDIRECT_URI
         )
 
-
         # Generate the authorization URL and redirect user to Google authorization URL
         authorization_url, _ = flow.authorization_url(prompt='consent')
         return redirect(authorization_url)
-       
 
 
 class GoogleCalendarRedirectView(APIView):
@@ -50,14 +48,28 @@ class GoogleCalendarRedirectView(APIView):
         )
 
         # Exchange the authorization code for access token
-        flow.fetch_token(code=code)
+        try:
+            flow.fetch_token(code=code)
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
         credentials = flow.credentials
 
         # Build the Google Calendar API service
-        service = build('calendar', 'v3', credentials=credentials)
+        try:
+            service = build('calendar', 'v3', credentials=credentials)
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
         # Get list of events from user's primary calendar
-        events = service.events().list(calendarId='primary', timeMin='2023-01-01T00:00:00Z', timeMax='2023-12-31T23:59:59Z').execute()
+        try:
+            events = service.events().list(
+                calendarId='primary',
+                timeMin='2023-01-01T00:00:00Z',
+                timeMax='2023-12-31T23:59:59Z'
+            ).execute()
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
         # Return the list of events in the response
         return Response(events)
